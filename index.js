2111ec9dff6888db439cc8959eb657761dedb330
georgegoldman <georgegoldman2014@gmail.com>
2021-08-10 10:08:10 +0100

    this is the first commit on this project

---
+++ index.js
@@ -0,0 +1,26 @@
+const restify = require('restify');
+const mongoose = require('mongoose');
+const config = require('./config');
+
+const server = restify.createServer();
+
+
+//middleware
+
+server.use(restify.plugins.bodyParser());
+
+server.listen(config.PORT, () => {
+    mongoose.connect(
+        config.MONGODB_URI,
+        { useNewUrlParser: true }
+    );
+});
+
+const db = mongoose.connection;
+
+db.on('error', error => console.error(error));
+
+db.once('open', () => {
+    require('./routes/customers')(server)
+    console.log(`Server started on port ${config.PORT}`);
+})
